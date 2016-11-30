fbb67b96df58b1f773efec53658a800d2ec252c9
Kohei Arai <tobecomebig@gmail.com>
2016-11-29 20:28:12 -0500

    Added file

--- HW4/TreeLabelWithoutStateMonad.hs
+++ HW4/TreeLabelWithoutStateMonad.hs
@@ -19,7 +19,6 @@ data Tree a = Nil | Node a (Tree a) (Tree a) deriving (Show,Eq)
 labelTree :: Ord a => Tree a -> (Store a Int) -> (Tree Int, Store a Int)
 labelTree Nil ls = (Nil, ls)
 -- Need to handle the first parameter is not 1
-  
 labelTree (Node val left right) ls =
   (Node labeledValue labeledLeft labeledRight, ls''')
     where (labeledValue, ls')   = labelValue val   ls
